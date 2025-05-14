3fe5376add01a8903ebcd2e6291e68828fbbc4d5
NaufalArdian12 <naufalopo87@gmail.com>
2025-05-14 16:29:07 +0700

    feat add logout in dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,13 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/auth/redirect', [SocialiteController::class, 'redirect'])->name('auth.redirect');
 
 Route::get('/auth/google/callback', [SocialiteController::class, 'callback']);
+
+Route::post('/logout', [SocialiteController::class, 'logout'])->name('logout');
+
