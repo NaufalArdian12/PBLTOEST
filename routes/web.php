dcd6f483cd9ffe4bd49f2a179e21611c90de9e4d
Binaa <sabrinarahmadini60@gmail.com>
2025-05-15 05:16:07 +0700

    add verification via email

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use App\Http\Controllers\Auth\SocialiteController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes(['verify'=>true]);
+
 Route::get('/auth/redirect', [SocialiteController::class, 'redirect']);
 
 Route::get('/auth/google/callback', [SocialiteController::class, 'callback']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
