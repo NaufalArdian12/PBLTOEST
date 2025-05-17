cdd0f97b7d6443ae8689f3f379c3fc6b900d5894
Binaa <sabrinarahmadini60@gmail.com>
2025-05-17 11:23:43 +0700

    add verification via gmail, register form

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -31,7 +31,7 @@
             <div class="hidden md:flex items-center">
                 <a href="{{ route('login') }}"
                     class="text-blue-600 border border-blue-600 hover:bg-blue-600 hover:text-white px-4 py-2 rounded-md text-sm font-medium">Login</a>
-                <a href=""
+               <a href="{{ route('register') }}"
                     class="ml-4 bg-blue-600 text-white hover:bg-blue-700 px-4 py-2 rounded-md text-sm font-medium">Sign
                     Up</a>
             </div>
