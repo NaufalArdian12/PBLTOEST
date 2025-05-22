1dba313764e6bb113e6dede5b5556627fb7f172c
NaufalArdian12 <naufalopo87@gmail.com>
2025-05-22 08:11:18 +0700

    dashboard after login

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Models\UserModels;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
