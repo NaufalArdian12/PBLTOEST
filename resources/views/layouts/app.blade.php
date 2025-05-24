7791c9cd2062175f9edc1059bfdf90d7f3fa42ae
NaufalArdian12 <naufalopo87@gmail.com>
2025-05-24 14:12:58 +0700

    fix: verify email view with tailwindcss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-=======
 <html lang="en">
 
->>>>>>> e1253e9b29705f0ebb0ce30325b8a5a93925a030
 <head>
     <meta charset="UTF-8">
     <title>Toest App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    @vite('resources/css/app.css') <!-- Laravel Vite integration -->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
@@ -21,26 +17,4 @@
         @yield('content')
     </main>
 </body>
-<<<<<<< HEAD
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Toest App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
-</head>
-
-<body>
-    <main class="py-4">
-        @yield('content')
-    </main>
-</body>
-</html>
-=======
-
-</html>
->>>>>>> e1253e9b29705f0ebb0ce30325b8a5a93925a030
