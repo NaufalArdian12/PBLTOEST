eb2b10d0ab99caeacc36731c1b1ab8a2e29e218e
NaufalArdian12 <naufalopo87@gmail.com>
2025-05-24 14:11:50 +0700

    fix view for dashbooard mahasiswa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 import './bootstrap';
-=======
 import $ from 'jquery';
 import 'datatables.net'; // versi default, tanpa bootstrap
 import 'datatables.net-dt/css/dataTables.dataTables.css';
@@ -13,6 +10,3 @@ $(function () {
 
 window.$ = $;
 window.jQuery = $;
->>>>>>> Stashed changes
-=======
->>>>>>> e1253e9b29705f0ebb0ce30325b8a5a93925a030
