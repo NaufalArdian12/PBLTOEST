fd507a2a99bb5df38cbbb7945c941ab817f7cdc1
NaufalArdian12 <naufalopo87@gmail.com>
2025-06-10 13:46:01 +0700

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\Admin\StudyProgramController;
 use App\Http\Controllers\Admin\ToeicTestController;
 use App\Http\Controllers\Mahasiswa\RegistrationController;
 use App\Http\Controllers\Mahasiswa\EnrollmentController;
+use App\Http\Controllers\ProfileController;
 
 // Public Routes
 Route::get('/', fn() => view('welcome'))->name('home');
@@ -34,11 +35,6 @@ Route::get('/auth/google/callback', [SocialiteController::class, 'callback']);
 
 // Admin Role Routes (no prefix)
 Route::middleware(['auth', 'verified', 'role:Admin'])->group(function () {
-    Route::get('/profile', [AdminController::class, 'profile'])->name('profile');
-    Route::patch('/registration/{id}/reject', [RegistrationApprovalController::class, 'reject'])->name('registration.reject');
-    Route::patch('/registration/{id}/approve', [RegistrationApprovalController::class, 'approve'])->name('registration.approve');
-
-    Route::get('/registration', [RegistrationApprovalController::class, 'index'])->name('registration.index');
 
     Route::get('/major', [MajorController::class, 'index'])->name('major.index');
     Route::post('/major/store_ajax', [MajorController::class, 'store_ajax'])->name('major.store');
@@ -46,9 +42,8 @@ Route::middleware(['auth', 'verified', 'role:Admin'])->group(function () {
     Route::get('/major/{id}/edit_ajax', [MajorController::class, 'edit'])->name('major.edit');
     Route::put('/major/{id}/update_ajax', [MajorController::class, 'update_ajax'])->name('major.update');
     Route::get('/major/create', [MajorController::class, 'create'])->name('major.create');
-    Route::get('/major/{id}/show_ajax', [MajorController::class, 'show_ajax']);
+    Route::get('/major/{id}/show_ajax', [MajorController::class, 'show'])->name('major.show');
 
-    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
 
     Route::get('/campus', [CampusController::class, 'index'])->name('campus.index');
     Route::delete('/campus/{id}/delete_ajax', [CampusController::class, 'confirm_ajax']);
@@ -61,14 +56,16 @@ Route::middleware(['auth', 'verified', 'role:Admin'])->group(function () {
     Route::get('/campus/{id}/edit', [CampusController::class, 'edit'])->name('campus.edit');
 
     Route::get('/student', [StudentController::class, 'index'])->name('student.index');
-    Route::post('/student/list', [StudentController::class, 'list']);
-    Route::post('/student/ajax', [StudentController::class, 'store_ajax']);
-    Route::get('/student/{id}/delete_ajax', [StudentController::class, 'confirm_ajax']);
-    Route::get('/student/{id}/edit_ajax', [StudentController::class, 'edit_ajax']);
-    Route::put('/student/{id}/update_ajax', [StudentController::class, 'update_ajax']);
+    Route::delete('/student/{id}', [StudentController::class, 'destroy'])->name('student.delete');
+    Route::get('/student/create', [StudentController::class, 'create'])->name('student.create');
+    Route::post('/student', [StudentController::class, 'store'])->name('student.store');
+    Route::get('/student/{id}/edit', [StudentController::class, 'edit'])->name('student.edit');
+    Route::put('/student/{id}/update', [StudentController::class, 'update'])->name('student.update');
     Route::delete('/student/{id}/delete_ajax', [StudentController::class, 'delete_ajax']);
-    Route::get('/student/create', [StudentController::class, 'create']);
-    Route::get('/student/{id}/show_ajax', [StudentController::class, 'show_ajax']);
+    Route::get('/student/{id}/show', [StudentController::class, 'show'])->name('student.show');
+    Route::get('/student/{id}/scan-ktp', [StudentController::class, 'showKtp'])->name('student.showKtp');
+    Route::get('/student/{id}/scan-ktm', [StudentController::class, 'showKtm'])->name('student.showKtm');
+    Route::get('/student/{id}/pas-photo', [StudentController::class, 'showPasPhoto'])->name('student.showPasPhoto');
 
     Route::get('/educationalstaff', [EduStaffController::class, 'index'])->name('educationalstaff.index');
     Route::post('/educationalstaff/list', [EduStaffController::class, 'list']);
@@ -81,30 +78,44 @@ Route::middleware(['auth', 'verified', 'role:Admin'])->group(function () {
     Route::get('/educationalstaff/{id}/show_ajax', [EduStaffController::class, 'show_ajax']);
 
     Route::get('/studyprogram', [StudyProgramController::class, 'index'])->name('studyprogram.index');
-    Route::post('/studyprogram/list', [StudyProgramController::class, 'list']);
-    Route::post('/studyprogram/ajax', [StudyProgramController::class, 'store_ajax']);
-    Route::get('/studyprogram/{id}/delete_ajax', [StudyProgramController::class, 'confirm_ajax']);
-    Route::get('/studyprogram/{id}/edit_ajax', [StudyProgramController::class, 'edit_ajax']);
-    Route::put('/studyprogram/{id}/update_ajax', [StudyProgramController::class, 'update_ajax']);
-    Route::delete('/studyprogram/{id}/delete_ajax', [StudyProgramController::class, 'delete_ajax']);
-    Route::get('/studyprogram/create', [StudyProgramController::class, 'create']);
-    Route::get('/studyprogram/{id}/show_ajax', [StudyProgramController::class, 'show_ajax']);
+    Route::delete('/studyprogram/{id}/', [StudyProgramController::class, 'destroy'])->name('studyprogram.delete');
+    Route::get('/studyprogram/create', [StudyProgramController::class, 'create'])->name('studyprogram.create');
+    Route::post('/studyprogram/', [StudyProgramController::class, 'store'])->name('studyprogram.store');
+    Route::get('/studyprogram/{id}/edit', [StudyProgramController::class, 'edit'])->name('studyprogram.edit');
+    Route::put('/studyprogram/{id}/update', [StudyProgramController::class, 'update'])->name('studyprogram.update');
+    Route::get('/studyprogram/{id}/show', [StudyProgramController::class, 'show'])->name('studyprogram.show');
 
     Route::get('/toeic-test', [ToeicTestController::class, 'index'])->name('toeic.index');
-    Route::post('/toeic-test/list', [ToeicTestController::class, 'list']);
-    Route::post('/toeic-test/ajax', [ToeicTestController::class, 'store_ajax']);
-    Route::get('/toeic-test/{id}/delete_ajax', [ToeicTestController::class, 'confirm_ajax']);
-    Route::get('/toeic-test/{id}/edit_ajax', [ToeicTestController::class, 'edit_ajax']);
+    Route::post('/toeic-test/', [ToeicTestController::class, 'store'])->name('toeic.store');
+    Route::get('/toeic-test/{id}/edit_ajax', [ToeicTestController::class, 'edit_ajax'])->name('toeic.edit');
     Route::put('/toeic-test/{id}/update_ajax', [ToeicTestController::class, 'update_ajax']);
-    Route::delete('/toeic-test/{id}/delete_ajax', [ToeicTestController::class, 'delete_ajax']);
-    Route::get('/toeic-test/create', [ToeicTestController::class, 'create']);
+    Route::delete('/toeic-test/{id}/delete_ajax', [ToeicTestController::class, 'delete_ajax'])->name('toeic.destroy');
+    Route::get('/toeic-test/create', [ToeicTestController::class, 'create'])->name('toeic.create');
     Route::get('/toeic-test/{id}/show_ajax', [ToeicTestController::class, 'show_ajax']);
+
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
+    Route::post('/admin/store', [AdminController::class, 'store'])->name('admin.store');
+    Route::get('/admin/{id}/show', [AdminController::class, 'show'])->name('admin.show');
+    Route::put('/admin/{id}/update', [AdminController::class, 'update'])->name('admin.update');
+    Route::get('/admin/{id}/edit', [AdminController::class, 'edit'])->name('admin.edit');
+    Route::delete('/admin/{id}/', [AdminController::class, 'delete_ajax'])->name('admin.delete');
+
+    //registration
+    Route::get('/registration', [RegistrationApprovalController::class, 'index'])->name('registration.index');
+    Route::patch('/registration/{id}/reject', [RegistrationApprovalController::class, 'reject'])->name('registration.reject');
+    Route::patch('/registration/{id}/approve', [RegistrationApprovalController::class, 'approve'])->name('registration.approve');
+    Route::get('/registration/{id}/show', [RegistrationApprovalController::class, 'show'])->name('registration.show');
+    Route::get('/registration/{id}/edit', [RegistrationApprovalController::class, 'edit'])->name('registration.edit');
+    Route::put('/registration/{id}/', [RegistrationApprovalController::class, 'update'])->name('registration.update');
+    Route::get('/registration/create', [RegistrationApprovalController::class, 'create'])->name('registration.create');
+    Route::post('/registration/store', [RegistrationApprovalController::class, 'store'])->name('registration.store');
+    Route::get('/registration/{id}/delete_ajax', [RegistrationApprovalController::class, 'delete'])->name('registration.delete');
 
 });
 
 // Student Role Routes (no prefix)
 Route::middleware(['auth', 'verified', 'role:Student'])->group(function () {
-    Route::get('/profile', [MahasiswaController::class, 'profile'])->name('profile');
     Route::get('/sertifikat', [MahasiswaController::class, 'sertifikat'])->name('sertifikat');
     Route::get('/pendaftaran', [EnrollmentController::class, 'create'])->name('pendaftaran.create');
     Route::post('/pendaftaran', [EnrollmentController::class, 'store'])->name('pendaftaran.store');
@@ -114,14 +125,19 @@ Route::middleware(['auth', 'verified', 'role:Student'])->group(function () {
 
 // Universal Authenticated Routes
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/set-password', [VerificationController::class, 'showSetPasswordForm'])->name('password.set');
-    Route::post('/set-password', [VerificationController::class, 'storePassword'])->name('password.store');
+    Route::get('/set-profile', [VerificationController::class, 'showSetPasswordForm'])->name('password.set');
+    Route::post('/set-profile', [VerificationController::class, 'storePassword'])->name('password.store');
 
     // Gunakan controller yang sama dengan logika berbeda
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->name('dashboard')
-    ->middleware(['auth', 'verified']);
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard')
+        ->middleware(['auth', 'verified']);
 
-// Di DashboardController:
+    Route::get('download-ktp/{filename}', [StudentController::class, 'downloadKtp'])->middleware(['auth', 'verified'])->name('download.ktp');
+
+    //gunakan controller profile
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile')->middleware(['auth', 'verified']);
+
+    // Di DashboardController:
 
 });
